30d42d60f09e84f895dda2752e761e2d078d6198
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-16 00:57:54 -0300

    4 - MOdelagem API Catalogo

---
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace NSE.Cliente.API.Models
+{
+    public class Cliente
+    {
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+    }
+
+    public class Endereco
+    {
+    }
+}
