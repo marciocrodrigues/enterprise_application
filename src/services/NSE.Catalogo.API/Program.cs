0ac110bf4c9184ca71372c76d40b17b7396e8118
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-17 01:07:44 -0300

    5.5 - Acesso a dados API Catalogo

--- src/services/NSE.Catalogo.API/Program.cs
+++ src/services/NSE.Catalogo.API/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Data;
+using NSE.Catalogo.API.Data.Repository;
+using NSE.Catalogo.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CatalogoContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<CatalogoContext>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
