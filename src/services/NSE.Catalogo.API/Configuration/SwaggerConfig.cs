ac81f163f5a0c962a2037eb5b17a1612f2da51a0
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-20 22:19:15 -0300

    5.8 - Validacao de acesso via claims

--- src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -15,6 +15,31 @@ namespace NSE.Catalogo.API.Configuration
                     Contact = new OpenApiContact() { Name = "Marcio", Email = "teste@teste.com.br" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/lecenses/MIT") }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insir o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
 
             return services;
