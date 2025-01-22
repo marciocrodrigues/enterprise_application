12828c5472d8d1aa9c46d2e70e643d1689797c44
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-21 23:37:04 -0300

    3.3 - Configurando a autenticacao

---
+++ src/web/NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIndetityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login"; // Quando não lgoado
+                    options.AccessDeniedPath = "/acesso-negado"; // Quando acesso negado
+                });
+
+            return services;
+        }
+
+        public static WebApplication UseIdentityConfiguration(this WebApplication app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static IServiceCollection AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            return services;
+        }
+
+        public static WebApplication UseMvcConfiguration(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            return app;
+        }
+    }
+}
