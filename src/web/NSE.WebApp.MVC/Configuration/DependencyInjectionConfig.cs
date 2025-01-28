add16e962f27aad256aef2c242365bb4991e6765
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-28 14:05:42 -0300

    4.8 - Acessando dados do Usuario

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,5 @@
-﻿using NSE.WebApp.MVC.Service;
+﻿using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Service;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -7,6 +8,8 @@ namespace NSE.WebApp.MVC.Configuration
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
 
             return services;
         }
