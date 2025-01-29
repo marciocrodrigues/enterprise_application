1d7bd7e718ffde9e94b018c5729667a16cdccffd
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-29 12:27:00 -0300

    4.13 - Boas praticas com HttpServices pt 1

--- src/web/NSE.WebApp.MVC/Service/AutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Service/AutenticacaoService.cs
@@ -1,6 +1,4 @@
 ﻿using NSE.WebApp.MVC.Models;
-using System.Text;
-using System.Text.Json;
 
 namespace NSE.WebApp.MVC.Service
 {
@@ -15,52 +13,36 @@ namespace NSE.WebApp.MVC.Service
 
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
-            var loginContent = new StringContent(
-                JsonSerializer.Serialize(usuarioLogin),
-                Encoding.UTF8,
-                "application/json");
+            var loginContent = ObterConteudo(usuarioLogin, "application/json");
 
             var response = await _httpClient.PostAsync("https://localhost:44361/api/identidade/autenticar", loginContent);
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-            };
 
             if (!TratarErrosResponse(response))
             {
                 return new UsuarioRespostaLogin
                 {
-                    ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response.Content.ReadAsStringAsync(), options)
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
                 };
             }
 
-            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+            return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
 
         public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
         {
-            var usuarioRegistroContent = new StringContent(
-                JsonSerializer.Serialize(usuarioRegistro),
-                Encoding.UTF8,
-                "application/json");
+            var usuarioRegistroContent = ObterConteudo(usuarioRegistro, "application/json");
 
             var response = await _httpClient.PostAsync("https://localhost:44361/api/identidade/nova-conta", usuarioRegistroContent);
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-            };
 
             if (!TratarErrosResponse(response))
             {
                 return new UsuarioRespostaLogin
                 {
-                    ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response.Content.ReadAsStringAsync(), options)
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
                 };
             }
 
-            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+            return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
     }
 }
--- src/web/NSE.WebApp.MVC/Service/Service.cs
+++ src/web/NSE.WebApp.MVC/Service/Service.cs
@@ -1,9 +1,31 @@
 ﻿using NSE.WebApp.MVC.Extensions;
+using System.Text.Json;
+using System.Text;
+using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Service
 {
     public abstract class Service
     {
+        protected StringContent ObterConteudo(object dado, string mediaType)
+        {
+            return new StringContent(
+                JsonSerializer.Serialize(dado),
+                Encoding.UTF8,
+                mediaType);
+        }
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
         protected bool TratarErrosResponse(HttpResponseMessage response)
         {
             switch ((int)response.StatusCode)
