8ab6842e18028b71481c36be73f7944d6eb8a594
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-01-30 21:53:15 -0300

    5.12 - Retry Pattern com Polly

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,8 @@
 ﻿using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Service;
 using NSE.WebApp.MVC.Service.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -10,15 +12,34 @@ namespace NSE.WebApp.MVC.Configuration
         {
             services.AddTransient<HttpClientAuthorizationDelegationHandler>();
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            //services.AddHttpClient<ICatalogoService, CatalogoService>()
-            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>(); // Intercepta quando houver request na classe adicionada no httpClient
 
-            services.AddHttpClient("Refit", options =>
-            {
-                options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
-            })
-            .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>() // Intercepta quando houver request na classe adicionada no httpClient
-            .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+            // Exemplo de policy
+            var retryWaitPolicy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                }, (outome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>() // Intercepta quando houver request na classe adicionada no httpClient
+                //.AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600))); 
+                .AddPolicyHandler(retryWaitPolicy);
+
+
+            //services.AddHttpClient("Refit", options =>
+            //{
+            //    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+            //})
+            //.AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>() // Intercepta quando houver request na classe adicionada no httpClient
+            //.AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
--- src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -5,9 +5,13 @@ namespace NSE.WebApp.MVC.Controllers
 {
     public class CatalogoController : MainController
     {
-        private readonly ICatalogoServiceRefit _catalogoService;
+        //private readonly ICatalogoServiceRefit _catalogoService;
+        private readonly ICatalogoService _catalogoService;
 
-        public CatalogoController(ICatalogoServiceRefit catalogoService)
+        public CatalogoController(
+            //ICatalogoServiceRefit catalogoService
+            ICatalogoService catalogoService
+            )
         {
             _catalogoService = catalogoService;
         }
