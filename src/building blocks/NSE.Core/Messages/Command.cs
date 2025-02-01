0a6344125106cb92590c53f6d6c667f6ad10aca6
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-02-01 13:38:24 -0300

    6.06 - Command e Command Handler

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult Valid { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
