063e673e56d7546bedd2317c86994813b0c6af23
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-02-01 11:28:25 -0300

    6.04 - Objetos de valor e validacoes

--- src/services/NSE.Cliente.API/Models/Cliente.cs
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -5,18 +5,29 @@ namespace NSE.Cliente.API.Models
     public class Cliente : Entity, IAggregateRoot
     {
         protected Cliente() { }
-        public Cliente(string nome, string email, string cpf)
+        public Cliente(Guid id, string nome, string email, string cpf)
         {
+            Id = id;
             Nome = nome;
-            Email = email;
-            Cpf = cpf;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
             Excluido = false;
         }
 
         public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
         public bool Excluido { get; private set; }
         public Endereco Endereco { get; private set; }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
     }
 }
