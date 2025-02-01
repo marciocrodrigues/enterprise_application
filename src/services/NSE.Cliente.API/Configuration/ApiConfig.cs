c2f32d15948dd07b36cc68f3489486c6b50b3343
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-02-01 12:22:14 -0300

    6.05 - Mapeamento das Tabelas

---
+++ src/services/NSE.Cliente.API/Configuration/ApiConfig.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Clientes.API.Data;
+using NSE.WebAPI.Core.Identidade;
+
+namespace NSE.Clientes.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ClientesContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total", builder =>
+                    builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+            return services;
+        }
+
+        public static WebApplicationBuilder AddSettingsConfiguration(this WebApplicationBuilder builder)
+        {
+            var env = builder.Environment.EnvironmentName;
+            builder.Host.ConfigureAppConfiguration((ctx, build) =>
+            {
+                build.SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings{env}.json", true, true)
+                .AddEnvironmentVariables();
+            });
+
+            return builder;
+        }
+
+        public static WebApplication UseApiConfiguration(this WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerConfiguration();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("Total");
+
+            app.UseAuthConfiguration();
+
+            app.MapControllers();
+
+            app.Run();
+
+            return app;
+        }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using NSE.Clientes.API.Data;
+
+namespace NSE.Clientes.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ClientesContext>();
+            return services;
+        }
+    }
+}
