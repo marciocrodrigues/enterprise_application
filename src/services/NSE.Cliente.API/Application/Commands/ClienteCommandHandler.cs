299dc029aca2a6a73c64c5fc352d21b02fd30e60
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-02-02 11:53:19 -0300

    6.08 - Persistencia via comand handler

--- src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -8,22 +8,30 @@ namespace NSE.Clientes.API.Application.Commands
     public class ClienteCommandHandler : CommandHandler,
         IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteCommandHandler(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
         public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
         {
             if (!message.EhValido()) return message.ValidationResult;
 
             var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
 
-            // Validacoes de negocio
+            var clienteExistente = await _clienteRepository.ObterPorCpf(cliente.Cpf.Numero);
 
-            // Persistir no banco!
-            if (true) // Já existe um cliente com o CPF informado
+            if (clienteExistente is not null)
             {
                 AdicionarErro("Este CPF já está em uso.");
                 return ValidationResult;
             }
 
-            return message.ValidationResult;
+            _clienteRepository.Adicionar(cliente);
+
+            return await PersistirDados(_clienteRepository.UnitOfWork);
         }
     }
 }
