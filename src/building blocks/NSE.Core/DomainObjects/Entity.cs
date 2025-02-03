2135a1ab53e69e105c4db33cc75e6f093e8465bd
Marcio Costa Rodrigues <40520700+marciocrodrigues@users.noreply.github.com>
2025-02-03 12:01:37 -0300

    6.10 - Trabalhando com Eventos

--- src/building blocks/NSE.Core/DomainObjects/Entity.cs
+++ src/building blocks/NSE.Core/DomainObjects/Entity.cs
@@ -1,9 +1,36 @@
-﻿namespace NSE.Core.DomainObjects
+﻿using NSE.Core.Messages;
+
+namespace NSE.Core.DomainObjects
 {
     public abstract class Entity
     {
         public Guid Id { get; set; }
 
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        private List<Event> _notificacoes;
+        public IReadOnlyCollection<Event> Notificacoes => _notificacoes?.AsReadOnly();
+
+        public void AdicionarEvento(Event evento)
+        {
+            _notificacoes = _notificacoes ?? new List<Event>();
+            _notificacoes.Add(evento);
+        }
+
+        public void RemoverEvento(Event eventItem)
+        {
+            _notificacoes?.Remove(eventItem);
+        }
+
+        public void LimparEventos()
+        {
+            _notificacoes?.Clear();
+        }
+
+        #region Comparações
         public override bool Equals(object? obj)
         {
             var compareTo = obj as Entity;
@@ -37,5 +64,6 @@
         {
             return $"{GetType().Name} [Id={Id}]";
         }
+        #endregion
     }
 }
